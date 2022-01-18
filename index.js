7aee47340ac05505360c72b2207a83d5053fc54b
Debjyoti Banerjee <debjyoti@b4b.ai>
2022-01-18 21:16:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+	res.send("Hello Server!!");
+});
+const PORT = precess.env.PORT || 5000;
+app.listen(PORT);
